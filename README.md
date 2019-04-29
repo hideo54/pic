a69eccd78f605be89ba6f22eb50d10240455b296
hideo54 <hideo54@hideo54.com>
2019-04-29 16:21:47 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# img.hideo54.com
+
+hideo54's ultra-nice image uploading/downloading server!
+
+## Startup
+
+1. `npm i`
+1. `node app.js /absolute/path/to/root/dir 3000`
+
+So if you want to use pm2, run this command:
+
+`pm2 start app.js (any other your favorite pm2 options) -- /absolute/path/to/root/dir 3000`
+
+## Upload
+
+To be implemented. Currently you need to place your image manually.
+
+## Download
+
+Let's say you want to download `background.png` in `icons/` directory, which original size is 1920x1080.
+
+### If you want to get the original image
+
+Just access to `/icons/main.png`. You can get the image of 1920x1080.
+
+### If you want to get the image which width is 720px
+
+Just access to `/icons/main-720w.png`. You can get the image of 720x405, the same aspect as the original.
+
+Now you must be getting the hang of the protocol. Yes, just access to `/icons/main-405h.png` if you want just to specify the height.
+
+### If you want to get the image which size is 720x300
+
+You know there are some style. Access to `/icons/main-720w-300h-{mode}.png` according to the mode you want. 
+
+There are 2 modes, which is listed below:
+
+* `contain` mode (default)
+
+This is the default mode, so if you just access to `/icons/main-720w-300h.png`, then this mode is applied.
+
+* `cover` mode
+
+## Todo
+
+* Support a method to specify the background color of the contain mode
+* Support jpg, webp
+* Support image uploading
+
+## Contact
+
+Mastodon: @hideo54@social.hideo54.com
+Twitter: @hideo54
+Email: contact@hideo54.com
---
+++ app.js
@@ -0,0 +1,76 @@
+const Koa = require('koa');
+const app = new Koa();
+const route = require('koa-route');
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
+
+const args = process.argv.slice(2);
+const rootDir = args[0];
+const port = args[1];
+
+app.use(async (ctx, next) => {
+    const elements = path.parse(ctx.path);
+    // Returns:
+    // { root: '/',
+    //   dir: '/home/user/dir',
+    //   base: 'file.txt',
+    //   ext: '.txt',
+    //   name: 'file' }
+    console.log(ctx.path);
+    if (elements.ext === '.png') {
+        const splitted = elements.name.split('-')
+        const originalFilename = `${rootDir}${elements.dir}/${splitted[0]}.png`;
+        const options = splitted.slice(1);
+        console.log(originalFilename);
+        try {
+            fs.accessSync(originalFilename, fs.constants.R_OK);
+            const widthDesc = options.filter(/./.test.bind(/^[0-9]+w$/));
+            if (widthDesc.length > 2) {
+                ctx.status = 400;
+                ctx.body = 'You have to specify the width only once.';
+            }
+            const heightDesc = options.filter(/./.test.bind(/^[0-9]+h$/));
+            if (heightDesc.length > 2) {
+                ctx.status = 400;
+                ctx.body = 'You have to specify the height only once.';
+            }
+            const width = widthDesc.length === 0 ? undefined : parseInt(widthDesc[0].slice(0, -1));
+            const height = heightDesc.length === 0 ? undefined : parseInt(heightDesc[0].slice(0, -1));
+            if (options.includes('cover')) {
+                if (options.includes('contain')) {
+                    ctx.status = 400;
+                    ctx.body = 'You have to decide either contain mode or cover mode.';
+                } else {
+                    // cover mode
+                    ctx.status = 200;
+                    ctx.type = 'image/png';
+                    ctx.body = await sharp(originalFilename)
+                        .resize(width, height, { fit: 'cover' })
+                        .png()
+                        .toBuffer();
+                }
+            } else {
+                // contain mode (even unless the mode is specified, because the contain mode is the default)
+                ctx.status = 200;
+                ctx.type = 'image/png';
+                const backgroundColor = { r: 255, g: 255, b: 255, alpha: 0 }; // TEMPORARY. The method to change this param is not implemented yet.
+                ctx.body = await sharp(originalFilename)
+                    .resize(width, height, {
+                        fit: 'contain',
+                        background: { r: 255, g: 255, b: 255, alpha: 0 }
+                    })
+                    .png()
+                    .toBuffer();
+            }
+        } catch (err) {
+            ctx.status = 400;
+            ctx.body = "No such file.";
+        }
+    } else {
+        ctx.status = 400;
+        ctx.body = 'Currently only .png is supported.';
+    }
+});
+
+app.listen(port);
