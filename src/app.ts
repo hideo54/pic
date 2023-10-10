e273e5a26f413767794d1273bcb344c51a6b6c6b
Hideo Yasumoto <hideo54@hideo54.com>
2023-10-10 15:56:54 +0900

    Minimum Cloud Run project

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import Fastify from 'fastify';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = Fastify();
+app.get('/', async (request, reply) => {
+    reply.send({
+        message: 'Sorry, this fox is LoveLive! only.',
+    });
+});
+
+app.listen({
+    host: '::',
+    port: Number(process.env.PORT) || 8080,
+});
